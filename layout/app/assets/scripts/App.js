d52f07f7b9dba162bf17e9037f72c8b23a92cfef
Anatolii <anatolii.lukavenko@gmail.com>
2018-12-19 15:39:47 +0200

    JS: counter

--- layout/app/assets/scripts/App.js
+++ layout/app/assets/scripts/App.js
@@ -1,20 +1,22 @@
 // 3rd party packages from NPM
 
 // Our modules / classes 
-import CopyrightYear 	from './modules/copyright-year';
-import UIToTop 				from './modules/ui-to-top';
-import Swiper 				from './modules/swiper';
-import ToggleMenu 		from './modules/toggle-menu';
-import Preloader  		from './modules/preloader';
-import Wow			  		from './modules/wow';
+import CopyrightYear 				from './modules/copyright-year';
+import UIToTop 							from './modules/ui-to-top';
+import Swiper 							from './modules/swiper';
+import ToggleMenu 					from './modules/toggle-menu';
+import Preloader  					from './modules/preloader';
+import Wow			  					from './modules/wow';
+import Counter 							from './modules/counter';
 
 // Instantiate a new object using our modules/classes
-var copyrightYear = new CopyrightYear();
-var uitotop 			= new UIToTop();
-var swiper 				= new Swiper();
-var toggleMenu 		= new ToggleMenu();
-var preloader 		= new Preloader();
-var wow 					= new Wow();
+var copyrightYear 			= new CopyrightYear();
+var uitotop 						= new UIToTop();
+var swiper 							= new Swiper();
+var toggleMenu 					= new ToggleMenu();
+var preloader 					= new Preloader();
+var wow 								= new Wow();
+var counterAnimate 			= new Counter();
 
 
 
--- layout/gulp/tasks/coreJs.js
+++ layout/gulp/tasks/coreJs.js
@@ -7,7 +7,8 @@ gulp.task('coreJs', function() {
 		'./app/assets/libs/Jquery_2.2.4/jquery_2.24.min.js',
 		'./app/assets/libs/UIToTop/ui-to-top.js',
 		'./app/assets/libs/Swiper_3.4.2/Swiper.js',
-		'./app/assets/libs/WOW/wow.js'
+		'./app/assets/libs/WOW/wow.js',
+		'./app/assets/libs/CountTo/count-to.js'
 		])
 	.pipe(concat('core.min.js'))
 	// .pipe(uglify()) // minimize
